36988dbe2f2c441b4b1216277aea1a21109f2fe0
Ryota <ryoenoki2013@gmail.com>
2023-01-25 17:01:31 +0900

    state

--- lib/cubit/weather_state.dart
+++ lib/cubit/weather_state.dart
@@ -1,6 +1,45 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'weather_cubit.dart';
 
 @immutable
-abstract class WeatherState {}
+abstract class WeatherState {
+  const WeatherState();
+}
 
-class WeatherInitial extends WeatherState {}
+class WeatherInitial extends WeatherState {
+  const WeatherInitial();
+}
+
+class WeatherLoading extends WeatherState {
+  const WeatherLoading();
+}
+
+class WeatherLoaded extends WeatherState {
+  const WeatherLoaded(this.weather);
+  final Weather weather;
+
+  @override
+  bool operator ==(covariant WeatherLoaded other) {
+    if (identical(this, other)) return true;
+
+    return other.weather == weather;
+  }
+
+  @override
+  int get hashCode => weather.hashCode;
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+  const WeatherError(this.message);
+
+  @override
+  bool operator ==(covariant WeatherError other) {
+    if (identical(this, other)) return true;
+
+    return other.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
