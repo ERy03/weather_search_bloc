348e2e00d4947a683ff590990b73e732102a958d
Ryota <ryoenoki2013@gmail.com>
2023-01-25 17:23:57 +0900

    cubit

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:weather_search_bloc/model/weather.dart';
+import 'package:weather_search_bloc/repository/weather_repository.dart';
 
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  final WeatherRepository _weatherRepository;
+
+  WeatherCubit(this._weatherRepository) : super(const WeatherInitial());
+
+  Future<void> fetchWeather(String cityName) async {
+    try {
+      emit(const WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    } on NetworkException {
+      emit(
+          const WeatherError('Could not fetch weather. Is the device online?'));
+    }
+  }
 }
